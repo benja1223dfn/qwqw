edd2091fbb8a8ab7e26e361e6f2f80e73286ac18
benja1223dfn <159678296+benja1223dfn@users.noreply.github.com>
2024-02-11 02:23:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function respuesta(respuesta) {
+    if (respuesta === 'si') {
+        window.location.href = 'te-amo.html';
+    } else if (respuesta === 'no') {
+        window.location.href = 'gato-enojado-full.html';
+    }
+}
